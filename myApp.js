5722c721250be4cc8737b212fb467ea337b8cae2
shewayou <jal8113@yahoo.com>
2024-07-14 16:18:28 -0400

    Update myApp.js to listen only once, at the end

--- myApp.js
+++ myApp.js
@@ -5,54 +5,6 @@ const PORT = process.env.PORT || 3030;
 const app = express();
 
 
-app.listen( PORT, () => {
-  console.log( `:brocoli:Useful Programmer Info Security App Started on port ${PORT}`);
-} );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -65,7 +17,8 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
-});
+
+
+app.listen( PORT, () => {
+  console.log( `:brocoli:Useful Programmer Info Security App Started on port ${PORT}`);
+} );
