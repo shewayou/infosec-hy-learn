a595e457e0c53778513b7839e1bd940d9c393c6d
shewayou <jal8113@yahoo.com>
2024-07-15 13:29:44 -0400

    change to myApp.js for Content-Security-Policy frame-ancestors "none"

--- myApp.js
+++ myApp.js
@@ -19,6 +19,16 @@ app.use(helmet.hidePoweredBy());
 
 app.use(helmet.frameguard( { action: 'deny' }));
 
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: { "frame-ancestors": 'none', },
+    },
+  })
+);
+
+
+
 app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
